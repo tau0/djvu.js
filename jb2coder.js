06e58afa6b56d6f3e3e68557e293dad1d6627cb3
Sukhanov <reload3k@gmail.com>
2014-05-02 15:31:11 +0400

    jb2 reader began

--- jb2coder.js
+++ jb2coder.js
@@ -14,6 +14,21 @@ define(['./zpcoder', './lib', './error'], function (zpcoder, lib, error) {
     this.recordType = new zpcoder.ZPNumContext(0, 0); 
     this.first = new JB2Rect(-1, 0, 0, 1);
     this.lineCounter = 0; 
+    
+    this.reset = function () {
+      this.recordType.reset();
+      this.imageSize.reset();
+      this.matchingSymbolIndex.reset();
+      this.symbolColumnNumver.reset();
+      this.symbolRowNumber.reset();
+      this.sameLineRowOffset.reset();
+      this.sameLineColumnOffset.reset();
+      this.newLineRowOffset.reset();
+      this.newLineColumnOffset.reset();
+      this.commentOctet.reset();
+      this.commentLength.reset();
+      this.requiredDictionaySize.reset();
+    };
   };
 
   var JB2Rect = function (left, top, width, height) {
