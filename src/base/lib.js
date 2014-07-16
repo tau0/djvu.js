81e16de3d4daee686aa5c4491b7392fb508714c4
Sukhanov <reload3k@gmail.com>
2014-07-16 23:06:57 +0400

    a lot Max's optimizations

--- src/base/lib.js
+++ src/base/lib.js
@@ -14,67 +14,33 @@ var lib = {
   },
   // TODO: Some buggy magic.
   toUnsignedShort: function (a) {
-    return (a % 65536 + 65536) % 65536;
-  },
-  initString : function (n) {
-    n = Math.round(n);
-    var ans = "", s = "0";
-    while (n) {
-      if (n % 2 == 1) {
-        ans = ans.concat(s);
-      }
-      n = Math.floor(n / 2);
-      s = s.concat(s);
-    }
-    return ans;
-  },
-  UsualArray : function (n) {
-    var data = [];
-    this.resize = function (n) {
-      var oldLength = data.length;
-      data.length = n;
-      for (var i = oldLength; i < data.length; ++i) {
-        data[i] = 0;
-      }
-    };
-    this.setBit = function (n, b) {
-      data[n] = b;
-    };
-    this.getBit = function (n) {
-      return data[n];
-    };
-    this.resize(n);
+    return (a & 0xFFFF + 65536) & 0xFFFF;
   },
   bitArray : function (n) {
     var data = [];
-    var bitNum = 32, bitNumLog = 5;
+    var bitNum = 32;
     this.resize = function (n) {
       var oldLength = data.length;
-      data.length = Math.ceil(n / bitNum);
+      data.length = Math.ceil(n / 32);
       for (var i = oldLength; i < data.length; ++i) {
         data[i] = 0;
       }
     };
     this.setBit = function (n, b) {
-      if (b) {
-        b = 1;
-      } else {
-        b = 0;
+      n = n | 0;
+      if (n < 0 || n >= data.length << 5) {
+        return;
       }
-      n = Math.round(n);
-      if (n < 0 || n >= data.length * bitNum) {
-        throw "bitArray: out of bounds";
-      }
-      data[n >> bitNumLog] |= 1 << (n % bitNum);
-      data[n >> bitNumLog] ^= 1 << (n % bitNum);
-      data[n >> bitNumLog] |= b << (n % bitNum);
+      data[n >> 5] |= 1 << (n & 0x1F);
+      data[n >> 5] ^= 1 << (n & 0x1F);
+      data[n >> 5] |= b << (n & 0x1F);
     };
     this.getBit = function (n) {
-      n = Math.round(n);
-      if (n < 0 || n >= data.length * bitNum) {
-        throw "bitArray: out of bounds";
+      n = n | 0;
+      if (n < 0 || n >= data.length * 32) {
+        return;
       }
-      return (data[n >> bitNumLog] >> (n % bitNum)) & 1;
+      return (data[n >> 5] >> (n & 0x1F)) & 1;
     };
     this.resize(n);
   }
